c62ebbef15d2ca4200b91ec0b58145db30205c4e
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2015-05-19 21:33:15 +0200

    Removed the class library.

---
+++ Src/ConsoleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleHost
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IEnumerable<string> query =
+                from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                select assembly.FullName;
+
+            foreach (string assembly in query)
+            {
+                Console.WriteLine(assembly);
+            }
+        }
+    }
+}
